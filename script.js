8f2a19d33f546f9564ccf01cc9279e2031690173
Dung Nguyen <nddung2812@gmail.com>
2022-08-02 23:24:31 +1000

    screen moves to left

---
+++ script.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+
+let gameSpeed = 5;
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = './Assets/layer-1.png'
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = './Assets/layer-2.png'
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = './Assets/layer-3.png'
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = './Assets/layer-4.png'
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = './Assets/layer-5.png'
+
+let x = 0;
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  ctx.drawImage(backgroundLayer4, x, 0)
+  x -= gameSpeed;
+  requestAnimationFrame(animate)
+  // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+animate()
